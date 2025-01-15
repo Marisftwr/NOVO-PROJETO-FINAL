18ecea204899c2f4b61204928272efa5e16daf8b
vargas.rita <159588529+FV-MariaRita@users.noreply.github.com>
2025-01-14 23:39:35 -0300

    Update PáginasDeLeituraLivro.php

--- PáginasDeLeituraLivro.php
+++ PáginasDeLeituraLivro.php
@@ -1,5 +1,30 @@
 <?php
-include('protect.php')
+include ('protect.php');
+
+if(isset($_GET['id'])) {
+  $id = $_GET['id'];
+
+  include('conexao.php');
+
+  $sql = "SELECT * FROM livro WHERE id=$id";
+  $query  = mysqli_query($conexao, $sql);
+
+  if(mysqli_num_rows($query) > 0){
+    $livroespecifico = mysqli_fetch_assoc($query);
+
+    $id = $livroespecifico['id'];
+    $nome = $livroespecifico['nome'];
+    $genero = $livroespecifico['genero'];
+    $autor = $livroespecifico['autor'];
+    $ano = $livroespecifico['ano'];
+    $numPag = $livroespecifico['numPaginas'];
+    $isbn = $livroespecifico['ISBN'];
+    $pathcapa = $livroespecifico['capa_path'];
+    $pathlivro = $livroespecifico['livro_path'];
+  } else {
+    echo "Livro não encontrado!";
+  }
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -8,12 +33,12 @@ include('protect.php')
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="IconSite.png">
     <link rel='stylesheet' href="bootstrap-grid.css">
-    <link rel='stylesheet' href="styles.css">
-    <link rel='stylesheet' href="bootstrap copy.css">
+    <link rel='stylesheet' href="styles1.css">
+    <link rel='stylesheet' href="bootstrap copy1.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Ahom&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
-    <script type="text/javascript" src="ClubeLiterarioProjetoFinal2.js"></script>
+    <script type="text/javascript" src="ClubeLiterarioProjetoFinal.js"></script>
     <script type="text/javascript" src="bootstrap.bundle.min.js" ></script>
 
     <title>Página de leitura - Nome do livro</title>
@@ -50,13 +75,28 @@ include('protect.php')
                   <div class="col-sm-1">
                     <button class="btn btn-outline-secondary" type="submit" id="" onclick="openNav()"><img id="x2" src="menu.png"></button>
                   </div>
-                  
+                  <div id="mySidenav" class="sidenav">
+                    <a class="quicksand-regular2" href="javascript:void(0)" class="closebtn" onclick="closeNav()"><img id="fechar" src="fechar.png"></a><br><br>
+                    <a href="PaginaDePerfil.php"><img id="ProfilePicNav" src="profile picture.jpg" class="rounded-circle me-3" alt="User Avatar"></a><br>
+                    <p class="quicksand-regular2"><?php echo $_SESSION['nome']; ?></p><br>
+                    <form class="form-inline">
+                    <input class="form-control col-sm-10" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit"><img id="BotaoPesquisa" src="SearchIcon1.png"></button>
+                    </form>
+                    <a class="quicksand-regular2" href="QuemSomos.html">Quem Somos?</a><br><br>
+                    <a class="quicksand-regular2" href="#">Minhas listas</a><br><br>
+                    <a class="quicksand-regular2" href="#">Avaliações</a><br><br>
+                    <a class="quicksand-regular2" href="#">Configurações</a><img id="config" src="configurações.png"><br><br>
+                    <a class="quicksand-regular2" href="#">Sair</a><br><br>
+                  </div>
                 </ul>
             </nav>
             <hr class="linhaHome"><br><br><br>
             <div class="container-fluid">
-               <div id="DivPags" style="height:100vh;" class="col-sm-6">
-                  <iframe height="100%" width=100% src='j-k-rowling-1-harry-potter-e-a-pedra-filosofal.pdf'></iframe>
+                <div class="container-fluid">
+                    <iframe src="<?php echo $pathlivro; ?>" id="pdf" ></iframe><br><br><br>
+                </div>
+                <h3 id="x17" class="quicksand-regular"> - Anotações:</h3><br>
                 </div><br><br>
                 <h3 id="x17" class="quicksand-regular"> Anotações:</h3><br>
                 <div class="card sm-1">
@@ -111,5 +151,32 @@ include('protect.php')
                     <a class="quicksand-regular2" href="#">Sair</a><br><br>
               </div>
 
+              <footer class="container-fluid"><br>
+        <h3 class="quicksand-regular">
+          Informações gerais:
+        </h3><br>
+        <h5 class="quicksand-regular">
+          Contato:
+        </h5>
+        <p  class="quicksand-regular2">
+          <img id="iconRedes" src="whatsapp.png">  (21)91989-1304
+        </p><br>
+        <p  class="quicksand-regular2">
+          <img id="iconRedes" src="google.png">  clubeliterario@gmail.com
+        </p><BR>
+        <h5   class="quicksand-regular">
+          Redes sociais:
+        </h5>
+        <p class="quicksand-regular2">
+          <img id="iconRedes" src="x-social-media-round-icon.png">  @Clube_literario
+        </p><br>
+        <p  class="quicksand-regular2">
+          <img id="iconRedes" src="Instagram.png">  @Clubeliterarioficial
+        </p><br>
+        <h5 class="quicksand-regular">
+          Endereço: 
+        </h5>
+        <p class="quicksand-regular2">Bairro, rua e número.</p><br>
+      </footer>
 </body>
 </html>
