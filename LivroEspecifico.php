cd277591a1f472ca30cb21f44aa1bac1dbd7257f
vargas.rita <159588529+FV-MariaRita@users.noreply.github.com>
2025-01-14 23:34:34 -0300

    Update LivroEspecifico.php

--- LivroEspecifico.php
+++ LivroEspecifico.php
@@ -1,6 +1,32 @@
 <?php
-include('protect.php')
+include ('protect.php');
+
+if(isset($_GET['id'])) {
+  $id = $_GET['id'];
+
+  include('conexao.php');
+
+  $sql = "SELECT * FROM livro WHERE id=$id";
+  $query  = mysqli_query($conexao, $sql);
+
+  if(mysqli_num_rows($query) > 0){
+    $livroespecifico = mysqli_fetch_assoc($query);
+
+    $id = $livroespecifico['id'];
+              $nome = $livroespecifico['nome'];
+              $genero = $livroespecifico['genero'];
+              $autor = $livroespecifico['autor'];
+              $ano = $livroespecifico['ano'];
+              $numPag = $livroespecifico['numPaginas'];
+              $isbn = $livroespecifico['ISBN'];
+              $pathcapa = $livroespecifico['capa_path'];
+              $pathlivro = $livroespecifico['livro_path'];
+  } else {
+    echo "Livro não encontrado!";
+  }
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <meta charset="UTF-8">
@@ -10,15 +36,15 @@ include('protect.php')
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="IconSite.png">
     <link rel='stylesheet' href="bootstrap-grid.css">
-    <link rel='stylesheet' href="styles.css">
-    <link rel='stylesheet' href="bootstrap copy.css">
+    <link rel='stylesheet' href="styles1.css">
+    <link rel='stylesheet' href="bootstrap copy1.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Ahom&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
-    <script type="text/javascript" src="ClubeLiterarioProjetoFinal2.js"></script>
+    <script type="text/javascript" src="ClubeLiterarioProjetoFinal.js"></script>
     <script type="text/javascript" src="bootstrap.bundle.min.js" ></script>
 
-    <title>Nome do Livro</title>
+    <title><?php echo $nome; ?></title>
 </head>
 <body>
     <body class="bodyRoxo">
@@ -48,15 +74,31 @@ include('protect.php')
   
                 </ul>
               </li>
-              <div class="col-sm-1">
+              <!--Nav lateral-->
+            <div class="col-sm-1">
             <button class="btn btn-outline-secondary" type="submit" id="" onclick="openNav()"><img id="x2" src="menu.png"></button>
             </div>
+            <div id="mySidenav" class="sidenav">
+              <a class="quicksand-regular2" href="javascript:void(0)" class="closebtn" onclick="closeNav()"><img id="fechar" src="fechar.png"></a><br><br>
+              <a href="PaginaDePerfil.php"><img id="ProfilePicNav" src="profile picture.jpg" class="rounded-circle me-3" alt="User Avatar"></a><br>
+              <p class="quicksand-regular2"><?php echo $_SESSION['nome']; ?></p><br>
+              <form class="form-inline">
+              <input class="form-control col-sm-10" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit"><img id="BotaoPesquisa" src="SearchIcon1.png"></button>
+              </form>
+              <a class="quicksand-regular2" href="QuemSomos.html">Quem Somos?</a><br><br>
+              <a class="quicksand-regular2" href="#">Minhas listas</a><br><br>
+              <a class="quicksand-regular2" href="#">Avaliações</a><br><br>
+              <a class="quicksand-regular2" href="configuracoes.php">Configurações</a><img id="config" src="configurações.png"><br><br>
+              <a href="logout.php" class="quicksand-regular2">Sair</a><br><br>
+            </div>
+            <!--Fim da nav lateral-->
             </ul>
         </nav>
         <hr class="linhaHome">
         <br><br>
         <div class="container-fluid">
-        <a href= "PáginasDeLeituraLivro.php"><img  id ="x14" src="HarryPotter1.jpg" alt="Livro1"></a> 
+        <a href= "PáginasDeLeituraLivro.php?id=<?php echo $id; ?>"><img  id ="x14" src="<?php echo $pathcapa;?>" alt="Livro1"></a> 
         </div><br>
         
         <div class="container-fluid">
@@ -87,7 +129,7 @@ include('protect.php')
               Gênero
             </h4>
             <h6 id="x17" class="quicksand-regular2">
-            Gênero do livro
+            <?php echo $genero; ?>
             </h6>
           </div>
           <div class="col-md-3">
@@ -95,7 +137,7 @@ include('protect.php')
               Autor
             </h4>
             <h6 id="x17" class="quicksand-regular2">
-            Nome do autor
+            <?php echo $autor; ?>
             </h6>
           </div>
           <div class="col-md-3">
@@ -103,10 +145,9 @@ include('protect.php')
               Ano de lançamento
             </h4>
             <h6 id="x17" class="quicksand-regular2">
-            xxxx
+            <?php echo $ano; ?>
             </h6>
           </div>
-          
           
         </div>
         </div><br><br>
@@ -123,28 +164,13 @@ include('protect.php')
             <div class="d-flex gap-3">
                 <img src="profile picture.jpg" alt="User Avatar" class="user-avatar">
                 <div class="flex-grow-1">
-                    <textarea class="form-control comment-input" cols="30" rows="2" placeholder="Escreva um comentário"></textarea>
+                    <textarea class="form-control comment-input" cols="60" rows="2" placeholder="Escreva um comentário"></textarea>
                     <div class="mt-3 text-end">
                         <button class="btn-comment">Comentar</button>
                     </div>
                 </div>
             </div>
         </div>
-        <!--Nav lateral-->
-        <div id="mySidenav" class="sidenav">
-              <a class="quicksand-regular2" href="javascript:void(0)" class="closebtn" onclick="closeNav()"><img id="fechar" src="fechar.png"></a><br><br>
-              <a href="PaginaDePerfil.php"><img id="ProfilePicNav" src="profile picture.jpg" class="rounded-circle me-3" alt="User Avatar"></a><br>
-              <p class="quicksand-regular2"><?php echo $_SESSION['nome']; ?></p><br>
-              <form class="form-inline">
-              <input class="form-control col-sm-10" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit"><img id="BotaoPesquisa" src="SearchIcon1.png"></button>
-              </form>
-              <a class="quicksand-regular2" href="QuemSomos.html">Quem Somos?</a><br><br>
-              <a class="quicksand-regular2" href="paraLer.php">Minha lista</a><br><br>
-              <a class="quicksand-regular2" href="avaliacoes.php">Avaliações</a><br><br>
-              <a class="quicksand-regular2" href="configuracoes.php">Configurações</a><img id="config" src="configurações.png"><br><br>
-              <a href="logout.php" class="quicksand-regular2">Sair</a><br><br>
-            </div>
 
         <!-- Comments List -->
         <div class="comments-list">
@@ -207,7 +233,73 @@ include('protect.php')
         </div>
     </div>
 </div>
+        <!--Parte de comentários-->
+      <!---<div class="container-fluid">
+        <h3 x
+        id="x17" class="quicksand-regular">
+          Comentários sobre o livro:
+        </h3><br>
+              <div class="divComment"><br>
+                <div class="card sm-1">
+                  <div class="card-body">
+                      <form>
+                      <div class="sm-4">
+                        <label for="commentContent" class="form-label">
+                          <a href="PaginaDePerfil">
+                          <img src="https://via.placeholder.com/50" class="rounded-circle me-3" alt="User Avatar">
+                          </a>
+                        </label>
+                        <div class="container-fluid">
+                        <textarea class="form-control" id="commentContent" rows="2" COLS="200" placeholder="Comente aqui"></textarea>
+                      </div>
+                      </div>
+                      <button type="submit" class="btn btn-outline-secondary">Post Comment</button>
+                    </form>
+                  </div>
         
+        </div><br><br>
+        Outro comentários além do usuário
+        <div class="comments-list">
+           Comment 1 
+          <div class="card mb-3">
+            <div id="x18" class="card-body">
+              <div class="d-flex mb-3">
+                <img src="https://via.placeholder.com/50" class="rounded-circle me-3" alt="User Avatar">
+                <div>
+                  <h5 class="card-title mb-0">João</h5>
+                  <small class="text-muted">Posted on July 17, 2024</small>
+                </div>
+              </div>
+              <p class="card-text">Adorei!</p>
+              <div class="d-flex justify-content-end">
+                <button class="btn btn-sm btn-outline-secondary me-2">
+                  <i class="bi bi-hand-thumbs-up"></i> Curtir
+                </button>
+                <button class="btn btn-sm btn-outline-secondary">
+                  <i class="bi bi-reply"></i> Responder
+                </button>
+              </div>
+              </div>
+            </div>
+            <div class="card mb-3">
+              <div id="x18" class="card-body">
+                <div class="d-flex mb-3">
+                  <img src="https://via.placeholder.com/50" class="rounded-circle me-3" alt="User Avatar">
+                  <div>
+                    <h5 class="card-title mb-0">Sarah</h5>
+                    <small class="text-muted">Posted on July 17, 2024</small>
+                  </div>
+                </div>
+                <p class="card-text">Que livro horrível!</p>
+                <div class="d-flex justify-content-end">
+                  <button class="btn btn-sm btn-outline-secondary me-2">
+                    <i class="bi bi-hand-thumbs-up"></i> Curtir
+                  </button>
+                  <button class="btn btn-sm btn-outline-secondary">
+                    <i class="bi bi-reply"></i> Responder
+                  </button>
+                </div>-->
+              
   </div>
     </div>
     <footer class="container-fluid"><br>
