a6b62a08562082cae362df33e0f8681c66f8f630
Mari <146585679+Marisftwr@users.noreply.github.com>
2025-01-16 23:47:25 -0300

    Update paraLer.php

--- paraLer.php
+++ paraLer.php
@@ -10,12 +10,12 @@ include('protect.php')
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="IconSite.png">
     <link rel='stylesheet' href="bootstrap-grid.css">
-    <link rel='stylesheet' href="styles.css">
-    <link rel='stylesheet' href="bootstrap copy.css">
+    <link rel='stylesheet' href="styles1.css">
+    <link rel='stylesheet' href="bootstrap copy2.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Ahom&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
-    <script type="text/javascript" src="ClubeLiterarioProjetoFinal2.js"></script>
+    <script type="text/javascript" src="ClubeLiterarioProjetoFinal1.js"></script>
     <script type="text/javascript" src="bootstrap.bundle.min.js" ></script>
     
     <title>Livros para ler</title>
@@ -48,16 +48,65 @@ include('protect.php')
                 <a href="PaginaDePerfil.php"><img id="ProfilePicNav" src="profile picture.jpg" class="rounded-circle me-3" alt="User Avatar"></a><br>
                 <p class="quicksand-regular2"><?php echo $_SESSION['nome']; ?></p><br>
               <form class="form-inline">
-              <input class="form-control col-sm-10" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit"><img id="BotaoPesquisa" src="SearchIcon1.png"></button>
+                <input class="form-control col-sm-3" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit"><img id="BotaoPesquisa" src="SearchIcon1.png"></button>
               </form>
-                <a class="quicksand-regular2" href="QuemSomos.html">Quem Somos?</a><br><br>
+                <a class="quicksand-regular2" href="QuemSomos.php">Quem Somos?</a><br><br>
                 <a class="quicksand-regular2" href="">Minha Lista</a><br><br>
                 <a class="quicksand-regular2" href="avaliacoes.php">Avaliações</a><br><br>
                 <a class="quicksand-regular2" href="EditarPerfil.php">Editar Perfil</a><br><br>
                 <a class="quicksand-regular2" href="#">Configurações</a><img id="config" src="configurações.png"><br><br>
                 <a class="quicksand-regular2" href="#">Sair</a><br><br>
             </div>
+
+            <div id="DivAv" class="row">
+              <div class="col-lg">
+                <div id="cardLivro" class="card" style="width: 15rem;">
+                    <img id="fotoLivro"  class="card-img-top" src="https://cdl-static.s3-sa-east-1.amazonaws.com/covers/gg/9788582851050/emma.jpg" alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">Emma</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg">
+                <div id="cardLivro" class="card" style="width: 15rem;">
+                    <img id="fotoLivro" style="height: 22rem;"  class="card-img-top" src="https://m.media-amazon.com/images/I/81eN74IRg4L._SL1297_.jpg" alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">Razão e sensibilidade</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg">
+                <div id="cardLivro" class="card" style="width: 15rem;">
+                    <img id="fotoLivro"  class="card-img-top" src="https://livrariapublica.com.br/capa/lady-susan-e-outras-historias-jane-austen-pdf-B085FXL6HM.webp" alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">Lady Susan</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg">
+                <div id="cardLivro" class="card" style="width: 15rem;">
+                    <img id="fotoLivro"class="card-img-top" src="HarryPotter1.jpg" alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">Título de livro</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br><br>
+    <div class="col-sm-2">
+    <button onclick="addLista()" class="btn btn-primary">Adicionar mais livros à lista</button>
+    </div><br><br>
+    <!--Div para pesquisar livros -->
+    <div id="PesqLivros"><br>
+      <h2 class="quicksand-regular">Qual livro deseja adicionar à sua lista?</h2><br>
+      <div class="search-container">
+        <form action="pesquisa.php" method="get">
+          <input class="form-control mr-sm-3" type="search" placeholder="Pesquise" name="busca">
+          <button id="BotaoPesquisa2" class="btn btn-primary" type="submit"><i class="fa fa-search"><img id="BotaoPesquisa" src="SearchIcon1.png"></i></button>
+        </form>
+      </div><br>
+    </div><br><br>
             <footer class="container-fluid"><br>
         <h3 class="quicksand-regular">
           Informações gerais:
