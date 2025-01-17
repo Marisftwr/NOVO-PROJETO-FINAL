bc27cf3a607d8b06bba971b0d86f33bfa58f07d6
Mari <146585679+Marisftwr@users.noreply.github.com>
2025-01-16 23:44:29 -0300

    Update LivroEspecifico.php

--- LivroEspecifico.php
+++ LivroEspecifico.php
@@ -37,11 +37,11 @@ if(isset($_GET['id'])) {
     <link rel="icon" type="image/x-icon" href="IconSite.png">
     <link rel='stylesheet' href="bootstrap-grid.css">
     <link rel='stylesheet' href="styles1.css">
-    <link rel='stylesheet' href="bootstrap copy1.css">
+    <link rel='stylesheet' href="bootstrap copy2.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Ahom&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
-    <script type="text/javascript" src="ClubeLiterarioProjetoFinal.js"></script>
+    <script type="text/javascript" src="ClubeLiterarioProjetoFinal1.js"></script>
     <script type="text/javascript" src="bootstrap.bundle.min.js" ></script>
 
     <title><?php echo $nome; ?></title>
@@ -74,25 +74,11 @@ if(isset($_GET['id'])) {
   
                 </ul>
               </li>
-              <!--Nav lateral-->
+           
             <div class="col-sm-1">
             <button class="btn btn-outline-secondary" type="submit" id="" onclick="openNav()"><img id="x2" src="menu.png"></button>
             </div>
-            <div id="mySidenav" class="sidenav">
-              <a class="quicksand-regular2" href="javascript:void(0)" class="closebtn" onclick="closeNav()"><img id="fechar" src="fechar.png"></a><br><br>
-              <a href="PaginaDePerfil.php"><img id="ProfilePicNav" src="profile picture.jpg" class="rounded-circle me-3" alt="User Avatar"></a><br>
-              <p class="quicksand-regular2"><?php echo $_SESSION['nome']; ?></p><br>
-              <form class="form-inline">
-              <input class="form-control col-sm-10" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit"><img id="BotaoPesquisa" src="SearchIcon1.png"></button>
-              </form>
-              <a class="quicksand-regular2" href="QuemSomos.html">Quem Somos?</a><br><br>
-              <a class="quicksand-regular2" href="#">Minhas listas</a><br><br>
-              <a class="quicksand-regular2" href="#">Avaliações</a><br><br>
-              <a class="quicksand-regular2" href="configuracoes.php">Configurações</a><img id="config" src="configurações.png"><br><br>
-              <a href="logout.php" class="quicksand-regular2">Sair</a><br><br>
-            </div>
-            <!--Fim da nav lateral-->
+            
             </ul>
         </nav>
         <hr class="linhaHome">
@@ -148,7 +134,7 @@ if(isset($_GET['id'])) {
             <?php echo $ano; ?>
             </h6>
           </div>
-          
+           
         </div>
         </div><br><br>
         <button href="ListasUser.php" class="btn btn-primary">Adicionar à sua lista</button><br><br>
@@ -232,73 +218,24 @@ if(isset($_GET['id'])) {
             </div>
         </div>
     </div>
-</div>
-        <!--Parte de comentários-->
-      <!---<div class="container-fluid">
-        <h3 x
-        id="x17" class="quicksand-regular">
-          Comentários sobre o livro:
-        </h3><br>
-              <div class="divComment"><br>
-                <div class="card sm-1">
-                  <div class="card-body">
-                      <form>
-                      <div class="sm-4">
-                        <label for="commentContent" class="form-label">
-                          <a href="PaginaDePerfil">
-                          <img src="https://via.placeholder.com/50" class="rounded-circle me-3" alt="User Avatar">
-                          </a>
-                        </label>
-                        <div class="container-fluid">
-                        <textarea class="form-control" id="commentContent" rows="2" COLS="200" placeholder="Comente aqui"></textarea>
-                      </div>
-                      </div>
-                      <button type="submit" class="btn btn-outline-secondary">Post Comment</button>
-                    </form>
-                  </div>
-        
-        </div><br><br>
-        Outro comentários além do usuário
-        <div class="comments-list">
-           Comment 1 
-          <div class="card mb-3">
-            <div id="x18" class="card-body">
-              <div class="d-flex mb-3">
-                <img src="https://via.placeholder.com/50" class="rounded-circle me-3" alt="User Avatar">
-                <div>
-                  <h5 class="card-title mb-0">João</h5>
-                  <small class="text-muted">Posted on July 17, 2024</small>
-                </div>
-              </div>
-              <p class="card-text">Adorei!</p>
-              <div class="d-flex justify-content-end">
-                <button class="btn btn-sm btn-outline-secondary me-2">
-                  <i class="bi bi-hand-thumbs-up"></i> Curtir
-                </button>
-                <button class="btn btn-sm btn-outline-secondary">
-                  <i class="bi bi-reply"></i> Responder
-                </button>
-              </div>
-              </div>
+</div>  
+<!--Nav lateral-->
+          <div id="mySidenav" class="sidenav">
+              <a class="quicksand-regular2" href="javascript:void(0)" class="closebtn" onclick="closeNav()"><img id="fechar" src="fechar.png"></a><br><br>
+              <a href="PaginaDePerfil.php"><img id="ProfilePicNav" src="profile picture.jpg" class="rounded-circle me-3" alt="User Avatar"></a><br>
+              <p class="quicksand-regular2"><?php echo $_SESSION['nome']; ?></p><br>
+              <form class="form-inline">
+              <input class="form-control col-sm-10" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit"><img id="BotaoPesquisa" src="SearchIcon1.png"></button>
+              </form>
+              <a class="quicksand-regular2" href="QuemSomos.php">Quem Somos?</a><br><br>
+              <a class="quicksand-regular2" href="paraLer.php">Minha lista</a><br><br>
+              <a class="quicksand-regular2" href="avaliacoes.php">Avaliações</a><br><br>
+              <a class="quicksand-regular2" href="configuracoes.php">Configurações</a><img id="config" src="configurações.png"><br><br>
+              <a href="logout.php" class="quicksand-regular2">Sair</a><br><br>
             </div>
-            <div class="card mb-3">
-              <div id="x18" class="card-body">
-                <div class="d-flex mb-3">
-                  <img src="https://via.placeholder.com/50" class="rounded-circle me-3" alt="User Avatar">
-                  <div>
-                    <h5 class="card-title mb-0">Sarah</h5>
-                    <small class="text-muted">Posted on July 17, 2024</small>
-                  </div>
-                </div>
-                <p class="card-text">Que livro horrível!</p>
-                <div class="d-flex justify-content-end">
-                  <button class="btn btn-sm btn-outline-secondary me-2">
-                    <i class="bi bi-hand-thumbs-up"></i> Curtir
-                  </button>
-                  <button class="btn btn-sm btn-outline-secondary">
-                    <i class="bi bi-reply"></i> Responder
-                  </button>
-                </div>-->
+<!--Fim da nav lateral-->
+          
               
   </div>
     </div>
