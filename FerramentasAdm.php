956832455eacb5a724d4e278d15466099e81d2cc
Mari <146585679+Marisftwr@users.noreply.github.com>
2025-01-16 23:42:45 -0300

    Update FerramentasAdm.php

--- FerramentasAdm.php
+++ FerramentasAdm.php
@@ -11,11 +11,11 @@ include('protect.php')
     <title>Clube literário - Home</title>
     <link rel='stylesheet' href="bootstrap-grid.css">
     <link rel='stylesheet' href="styles1.css">
-    <link rel='stylesheet' href="bootstrap copy1.css">
+    <link rel='stylesheet' href="bootstrap copy2.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Ahom&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
-    <script type="text/javascript" src="ClubeLiterarioProjetoFinal.js"></script>
+    <script type="text/javascript" src="ClubeLiterarioProjetoFinal1.js"></script>
     <script type="text/javascript" src="bootstrap.bundle.min.js" ></script>
 </head>
 <body class="bodyRoxo">
@@ -25,7 +25,7 @@ include('protect.php')
             <img id="x1" class="img-fluid" src="LogoSite3.png">
           </a>
           <li class="col-sm-2">
-            <a class="nav-link" href="Home.php">Home</a>
+            <a class="nav-link" href="HomeAdm.php">Home</a>
           </li>
             <li class="col-sm-2">
               <a class="nav-link" href="#">Destaques</a>
@@ -53,65 +53,101 @@ include('protect.php')
       </nav>
         <hr class="linhaHome">
         <br><br>
-        <h1 class="quicksand-regular">Bem vindo ao Clube Literario, <?php echo $_SESSION['nome']; ?></h1><br>
+        <!--<h1 class="quicksand-regular">Bem vindo ao Clube Literario, <?php echo $_SESSION['nome']; ?></h1><br>-->
         <!--Carrossel de fotos-->
         <div class="container-fluid">
         </div><br><br>
         <!--Nav lateral (coloquei aqui pois o carrossel estava atrapalhando)-->
             <div id="mySidenav" class="sidenav">
               <a class="quicksand-regular2" href="javascript:void(0)" class="closebtn" onclick="closeNav()"><img id="fechar" src="fechar.png"></a><br><br>
-              <a href="PaginaDePerfil.php"><img src="https://via.placeholder.com/50" class="rounded-circle me-3" alt="User Avatar"></a><br>
+              <a href="PaginaDePerfil.php"><img id="ProfilePicNav" src="profile picture.jpg" class="rounded-circle me-3" alt="User Avatar"></a><br>
               <p class="quicksand-regular2"><?php echo $_SESSION['nome']; ?></p><br>
               <form class="form-inline">
               <input class="form-control col-sm-10" type="search" placeholder="Search" aria-label="Search">
               <button class="btn btn-outline-success" type="submit"><img id="BotaoPesquisa" src="SearchIcon1.png"></button>
               </form>
-              <a class="quicksand-regular2" href="QuemSomos.html">Quem Somos?</a><br><br>
-              <a class="quicksand-regular2" href="#">Minhas listas</a><br><br>
-              <a class="quicksand-regular2" href="#">Avaliações</a><br><br>
+              <a class="quicksand-regular2" href="QuemSomos.php">Quem Somos?</a><br><br>
+              <a class="quicksand-regular2" href="paraLer.php">Minha lista</a><br><br>
+              <a class="quicksand-regular2" href="avaliacoes.php">Avaliações</a><br><br>
               <a class="quicksand-regular2" href="configuracoes.php">Configurações</a><img id="config" src="configurações.png"><br><br>
               <a class="quicksand-regular2" href="FerramentasAdm.php">Ferramentas do Administrador</a><img id="config" src="configurações.png"><br><br>
               <a href="logout.php" class="quicksand-regular2">Sair</a><br><br>
             </div>
       <!--Fim nav lateral-->
-      <div class="container-fluid" id="divLivros"><br>
+      <div class="container-fluid" id="divCads"><br>
           <h2 class="quicksand-regular">
             Controle dos livros do sistema
           </h2>
-          <button onclick="AdicionarLivros()" class="btn">Adicionar livros ao banco de dados</button><br><br>
-          <button onclick="AlterarLivros()" class="btn">Alterar livros no banco de dados</button><br><br>
-          <button onclick="ExcluirLivros()" class="btn">Excluir livros ao banco de dados</button><br><br>
+          <div id="btn-group" class="btn-group">
+            <button onclick="AdicionarLivros()" class="btn btn-secondary">Adicionar livros ao banco de dados</button><br><br>
+            <button onclick="AlterarLivros()" class="btn btn-secondary">Alterar livros no banco de dados</button><br><br>
+            <button onclick="ExcluirLivros()" class="btn btn-secondary">Excluir livros ao banco de dados</button><br><br>
+          </div>
+          <br><br>
       </div>
       <!-- Formukário para o Administrador adicionar livros ao sistema-->
       <div class="container-fluid" id="cadLivro"><br><br>
-        <h1 class="quicksand-regular" id="CadastroLivros">Cadastrar Livro</h1>
+        <h1 class="quicksand-regular" id="CadastroLivros">Cadastrar Livro</h1><br>
         <form method="post" action="cadastroLivros.php" class="quicksand-regular2" enctype="multipart/form-data">
+        <div class="row gx-3 mb-3">
+          <div class="col-md-3">
             <label>Nome do Livro</label>
             <input class="form-control" type="text" name="nomelivro" placeholder="Nome">
             <br><br>
+          </div>
+          <div class="col-md-3">
             <label>Gênero</label>
             <input class="form-control" type="text" name="genero" placeholder="Romance, Aventura, Suspense...">
             <br><br>
+          </div>
+          <div class="col-md-3">
             <label>Autor</label>
             <input class="form-control" type="text" name="autor" placeholder="Autor">
             <br><br>
+          </div>
+          <div class="col-md-3">
             <label>Ano de Lançamento</label>
             <input class="form-control" type="number" name="ano" placeholder="Ano">
             <br><br>
+          </div>
+        </div>
+        <div class="row gx-3 mb-4">
+          <div class="col-md-3">
             <label>Número de Páginas</label>
             <input class="form-control" type="number" name="numPag" placeholder="Numero de Paginas">
             <br><br>
+          </div>
+          <div class="col-md-3">
             <label>ISBN</label>
             <input class="form-control" type="text" name="isbn" placeholder="ISBN">
             <br><br>
-            <label>Capa do Livro (insira um link do google com a imagem da capa)</label>
+          </div>
+          <div class="col-md-3">
+            <label>Sinopse (limite 1500 caracteres)</label>
             <input class="form-control" type="text" name="capa">
             <br><br>
+          </div>
+          <div class="col-md-3">
+            <label>Qual a nacionalidade do livro?</label>
+            <input class="form-control" type="text" name="capa">
+            <br><br>
+          </div>
+        </div>
+        <div class="row gx-3 mb-4">
+          <div class="col-md-6">
+            <label>Capa do Livro (insira um link do google com a imagem da capa)</label>
+            <input class="form-control" type="text" name="arquivo">
+            <br><br>
+          </div>
+          <div class="col-md-6">
             <label>Arquivo PDF do livro</label>
             <input class="form-control" type="file" name="arquivo">
             <br><br>
+          </div>
+        </div>
             <button type="submit" value="Submit" class="btn btn-primary">Cadastrar o Livro</button><br><br>
-        </form>
+      </form>
+        
     </div>
 
     <!-- Formukário para o Administrador alterar livros ao sistema-->
@@ -120,33 +156,92 @@ include('protect.php')
         <form method="post" action="alterarLivros.php" class="quicksand-regular2" enctype="multipart/form-data">
             <label>Entre com o nome do livro no qual deseja realizar alterações</label>
             <input class="form-control" type="text" name="nomeLivro" placeholder="Nome do livro que deseja atualizar">
-            <br><br><br><br>
-            <label>Nome do Livro</label>
-            <input class="form-control" type="text" name="novonomeLivro" placeholder="Nome">
             <br><br>
+            <div class="row gx-3 mb-3">
+          <div class="col-md-3">
+            <label>Nome do Livro</label>
+            <input class="form-control" type="text" name="nomelivro" placeholder="Nome">
+            <br><br>
+          </div>
+          <div class="col-md-3">
             <label>Gênero</label>
             <input class="form-control" type="text" name="genero" placeholder="Romance, Aventura, Suspense...">
             <br><br>
+          </div>
+          <div class="col-md-3">
             <label>Autor</label>
             <input class="form-control" type="text" name="autor" placeholder="Autor">
             <br><br>
+          </div>
+          <div class="col-md-3">
             <label>Ano de Lançamento</label>
             <input class="form-control" type="number" name="ano" placeholder="Ano">
             <br><br>
+          </div>
+        </div>
+        <div class="row gx-3 mb-4">
+          <div class="col-md-3">
             <label>Número de Páginas</label>
             <input class="form-control" type="number" name="numPag" placeholder="Numero de Paginas">
             <br><br>
+          </div>
+          <div class="col-md-3">
             <label>ISBN</label>
             <input class="form-control" type="text" name="isbn" placeholder="ISBN">
             <br><br>
-            <label>Capa do Livro (insira um link do google com a imagem da capa)</label>
+          </div>
+          <div class="col-md-3">
+            <label>Sinopse (limite 1500 caracteres)</label>
             <input class="form-control" type="text" name="capa">
             <br><br>
+          </div>
+          <div class="col-md-3">
+            <label>Qual a nacionalidade do livro?</label>
+            <input class="form-control" type="text" name="capa">
+            <br><br>
+          </div>
+        </div>
+        <div class="row gx-3 mb-4">
+          <div class="col-md-6">
+            <label>Capa do Livro (insira um link do google com a imagem da capa)</label>
+            <input class="form-control" type="file" name="arquivo">
+            <br><br>
+          </div>
+          <div class="col-md-6">
             <label>Arquivo PDF do livro</label>
             <input class="form-control" type="file" name="arquivo">
             <br><br>
+          </div>
+        </div>
             <button type="submit" value="Submit" class="btn btn-primary">Alterar o Livro</button><br><br>
         </form>
     </div>
 </body>
+<footer class="container-fluid"><br>
+        <h3 class="quicksand-regular">
+          Informações gerais:
+        </h3><br>
+        <h5 class="quicksand-regular">
+          Contato:
+        </h5>
+        <p  class="quicksand-regular2">
+          <img id="iconRedes" src="whatsapp.png">  (21)91989-1304
+        </p><br>
+        <p  class="quicksand-regular2">
+          <img id="iconRedes" src="google.png">  clubeliterario@gmail.com
+        </p><BR>
+        <h5   class="quicksand-regular">
+          Redes sociais:
+        </h5>
+        <p class="quicksand-regular2">
+          <img id="iconRedes" src="x-social-media-round-icon.png">  @Clube_literario
+        </p><br>
+        <p  class="quicksand-regular2">
+          <img id="iconRedes" src="Instagram.png">  @Clubeliterarioficial
+        </p><br>
+        <h5 class="quicksand-regular">
+          Endereço: 
+        </h5>
+        <p class="quicksand-regular2">Bairro, rua e número.</p><br>
+</footer>
 </html>
